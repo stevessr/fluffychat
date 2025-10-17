027b36457b7f932a428a9853495ced5970847143
stevessr <steve-ssr@outlook.com>
2025-10-17 23:07:53 +0800

    emoji reaction

--- lib/pages/chat/events/message.dart
+++ lib/pages/chat/events/message.dart
@@ -3,14 +3,12 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:matrix/matrix.dart';
 import 'package:swipe_to_action/swipe_to_action.dart';
 
 import 'package:fluffychat/config/themes.dart';
 import 'package:fluffychat/l10n/l10n.dart';
 import 'package:fluffychat/pages/chat/events/room_creation_state_event.dart';
-import 'package:fluffychat/utils/adaptive_bottom_sheet.dart';
 import 'package:fluffychat/utils/date_time_extension.dart';
 import 'package:fluffychat/utils/file_description.dart';
 import 'package:fluffychat/utils/poll_extension.dart';
@@ -23,6 +21,7 @@ import 'message_content.dart';
 import 'message_reactions.dart';
 import 'reply_content.dart';
 import 'state_message.dart';
+import '../custom_reaction_dialog.dart';
 
 class Message extends StatelessWidget {
   final Event event;
@@ -113,20 +112,20 @@ class Message extends StatelessWidget {
         !event.originServerTs.sameEnvironment(nextEvent!.originServerTs);
     final nextEventSameSender = nextEvent != null &&
         ({
-          EventTypes.Message,
-          EventTypes.Sticker,
-          EventTypes.Encrypted,
-        }.contains(nextEvent!.type) ||
+              EventTypes.Message,
+              EventTypes.Sticker,
+              EventTypes.Encrypted,
+            }.contains(nextEvent!.type) ||
             nextEvent!.isPollStart) &&
         nextEvent!.senderId == event.senderId &&
         !displayTime;
 
     final previousEventSameSender = previousEvent != null &&
         ({
-          EventTypes.Message,
-          EventTypes.Sticker,
-          EventTypes.Encrypted,
-        }.contains(previousEvent!.type) ||
+              EventTypes.Message,
+              EventTypes.Sticker,
+              EventTypes.Encrypted,
+            }.contains(previousEvent!.type) ||
             previousEvent!.isPollStart) &&
         previousEvent!.senderId == event.senderId &&
         previousEvent!.originServerTs.sameEnvironment(event.originServerTs);
@@ -734,109 +733,28 @@ class Message extends StatelessWidget {
                                                               ).customReaction,
                                                               onPressed:
                                                                   () async {
-                                                                final emoji =
-                                                                    await showAdaptiveBottomSheet<
-                                                                        String>(
-                                                                  context:
-                                                                      context,
-                                                                  builder:
-                                                                      (context) =>
-                                                                          Scaffold(
-                                                                    appBar:
-                                                                        AppBar(
-                                                                      title:
-                                                                          Text(
-                                                                        L10n.of(context)
-                                                                            .customReaction,
-                                                                      ),
-                                                                      leading:
-                                                                          CloseButton(
-                                                                        onPressed:
-                                                                            () =>
-                                                                                Navigator.of(
-                                                                          context,
-                                                                        ).pop(
-                                                                          null,
-                                                                        ),
-                                                                      ),
-                                                                    ),
-                                                                    body:
-                                                                        SizedBox(
-                                                                      height: double
-                                                                          .infinity,
-                                                                      child:
-                                                                          EmojiPicker(
-                                                                        onEmojiSelected: (
-                                                                          _,
-                                                                          emoji,
-                                                                        ) =>
-                                                                            Navigator.of(
-                                                                          context,
-                                                                        ).pop(
-                                                                          emoji
-                                                                              .emoji,
-                                                                        ),
-                                                                        config:
-                                                                            Config(
-                                                                          emojiViewConfig:
-                                                                              const EmojiViewConfig(
-                                                                            backgroundColor:
-                                                                                Colors.transparent,
-                                                                          ),
-                                                                          bottomActionBarConfig:
-                                                                              const BottomActionBarConfig(
-                                                                            enabled:
-                                                                                false,
-                                                                          ),
-                                                                          categoryViewConfig:
-                                                                              CategoryViewConfig(
-                                                                            initCategory:
-                                                                                Category.SMILEYS,
-                                                                            backspaceColor:
-                                                                                theme.colorScheme.primary,
-                                                                            iconColor:
-                                                                                theme.colorScheme.primary.withAlpha(
-                                                                              128,
-                                                                            ),
-                                                                            iconColorSelected:
-                                                                                theme.colorScheme.primary,
-                                                                            indicatorColor:
-                                                                                theme.colorScheme.primary,
-                                                                            backgroundColor:
-                                                                                theme.colorScheme.surface,
-                                                                          ),
-                                                                          skinToneConfig:
-                                                                              SkinToneConfig(
-                                                                            dialogBackgroundColor:
-                                                                                Color.lerp(
-                                                                              theme.colorScheme.surface,
-                                                                              theme.colorScheme.primaryContainer,
-                                                                              0.75,
-                                                                            )!,
-                                                                            indicatorColor:
-                                                                                theme.colorScheme.onSurface,
-                                                                          ),
-                                                                        ),
-                                                                      ),
-                                                                    ),
-                                                                  ),
+                                                                final disabled =
+                                                                    sentReactions;
+                                                                final key =
+                                                                    await CustomReactionDialog
+                                                                        .show(
+                                                                  context,
+                                                                  event.room,
+                                                                  disabledKeys:
+                                                                      disabled,
                                                                 );
-                                                                if (emoji ==
-                                                                    null) {
-                                                                  return;
-                                                                }
-                                                                if (sentReactions
-                                                                    .contains(
-                                                                  emoji,
-                                                                )) {
+                                                                if (key ==
+                                                                        null ||
+                                                                    disabled
+                                                                        .contains(
+                                                                            key)) {
                                                                   return;
                                                                 }
                                                                 onSelect(event);
-
                                                                 await event.room
                                                                     .sendReaction(
                                                                   event.eventId,
-                                                                  emoji,
+                                                                  key,
                                                                 );
                                                               },
                                                             ),
