740e189a4c7161116940f18ad5725396dc22dbf7
stevessr <steve-ssr@outlook.com>
2026-03-05 09:00:04 +0800

    update

--- lib/pages/chat/events/message_download_content.dart
+++ lib/pages/chat/events/message_download_content.dart
@@ -1,5 +1,6 @@
 import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/config/setting_keys.dart';
+import 'package:fluffychat/l10n/l10n.dart';
 import 'package:fluffychat/utils/file_description.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/event_extension.dart';
 import 'package:fluffychat/utils/url_launcher.dart';
@@ -7,7 +8,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:matrix/matrix.dart';
 
-class MessageDownloadContent extends StatelessWidget {
+import 'media_spoiler_overlay.dart';
+
+class MessageDownloadContent extends StatefulWidget {
   final Event event;
   final Color textColor;
   final Color linkColor;
@@ -20,17 +23,50 @@ class MessageDownloadContent extends StatelessWidget {
   });
 
   @override
+  State<MessageDownloadContent> createState() => _MessageDownloadContentState();
+}
+
+class _MessageDownloadContentState extends State<MessageDownloadContent> {
+  bool _revealed = false;
+
+  bool get _isSpoiler => widget.event.isMediaSpoiler;
+
+  void _handleTap(BuildContext context) {
+    if (_isSpoiler && !_revealed) {
+      setState(() => _revealed = true);
+      return;
+    }
+    widget.event.saveFile(context);
+  }
+
+  @override
+  void didUpdateWidget(covariant MessageDownloadContent oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.event.eventId != widget.event.eventId) {
+      _revealed = false;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final filename = event.content.tryGet<String>('filename') ?? event.body;
+    final l10n = L10n.of(context);
+    final isObscured = _isSpoiler && !_revealed;
+    final spoilerReason = widget.event.mediaSpoilerReason;
+    final spoilerLabel = spoilerReason == null
+        ? l10n.spoilerText
+        : '${l10n.spoilerText}: $spoilerReason';
+
+    final filename =
+        widget.event.content.tryGet<String>('filename') ?? widget.event.body;
     final filetype = (filename.contains('.')
         ? filename.split('.').last.toUpperCase()
-        : event.content
+        : widget.event.content
                   .tryGetMap<String, Object?>('info')
                   ?.tryGet<String>('mimetype')
                   ?.toUpperCase() ??
               'UNKNOWN');
-    final sizeString = event.sizeString ?? '?MB';
-    final fileDescription = event.fileDescription;
+    final sizeString = widget.event.sizeString ?? '?MB';
+    final fileDescription = widget.event.fileDescription;
     return Column(
       mainAxisSize: .min,
       crossAxisAlignment: .start,
@@ -40,47 +76,61 @@ class MessageDownloadContent extends StatelessWidget {
           color: Colors.transparent,
           child: InkWell(
             borderRadius: BorderRadius.circular(AppConfig.borderRadius / 2),
-            onTap: () => event.saveFile(context),
-            child: Container(
-              width: 400,
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisSize: .min,
-                spacing: 16,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: textColor.withAlpha(32),
-                    child: Icon(Icons.file_download_outlined, color: textColor),
-                  ),
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: .start,
-                      mainAxisSize: .min,
-                      children: [
-                        Text(
-                          filename,
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            color: textColor,
-                            fontWeight: FontWeight.w500,
-                          ),
+            onTap: () => _handleTap(context),
+            child: Stack(
+              children: [
+                Container(
+                  width: 400,
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisSize: .min,
+                    spacing: 16,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: widget.textColor.withAlpha(32),
+                        child: Icon(
+                          Icons.file_download_outlined,
+                          color: widget.textColor,
                         ),
-                        Text(
-                          '$sizeString | $filetype',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(color: textColor, fontSize: 10),
+                      ),
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              filename,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: widget.textColor,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              '$sizeString | $filetype',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: widget.textColor,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                if (isObscured)
+                  Positioned.fill(
+                    child: MediaSpoilerOverlay(label: spoilerLabel),
+                  ),
+              ],
             ),
           ),
         ),
-        if (fileDescription != null) ...[
+        if (!isObscured && fileDescription != null) ...[
           Padding(
             padding: const EdgeInsets.symmetric(
               horizontal: 16.0,
@@ -90,19 +140,19 @@ class MessageDownloadContent extends StatelessWidget {
               text: fileDescription,
               textScaleFactor: MediaQuery.textScalerOf(context).scale(1),
               style: TextStyle(
-                color: textColor,
+                color: widget.textColor,
                 fontSize:
                     AppSettings.fontSizeFactor.value *
                     AppConfig.messageFontSize,
               ),
               options: const LinkifyOptions(humanize: false),
               linkStyle: TextStyle(
-                color: linkColor,
+                color: widget.linkColor,
                 fontSize:
                     AppSettings.fontSizeFactor.value *
                     AppConfig.messageFontSize,
                 decoration: TextDecoration.underline,
-                decorationColor: linkColor,
+                decorationColor: widget.linkColor,
               ),
               onOpen: (url) => UrlLauncher(context, url.url).launchUrl(),
             ),
