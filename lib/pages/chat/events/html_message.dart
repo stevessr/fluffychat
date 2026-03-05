29d3cd1dfb4b752c459fc23be456da78c9208080
stevessr <steve-ssr@outlook.com>
2026-03-05 11:10:39 +0800

    refactor(chat): unify media spoiler tap flow across media widgets
    Extract a reusable spoiler tap builder so image/video/file and HTML inline spoiler images share first-tap reveal and second-tap open behavior consistently.

--- lib/pages/chat/events/html_message.dart
+++ lib/pages/chat/events/html_message.dart
@@ -14,6 +14,7 @@ import 'package:fluffychat/utils/event_checkbox_extension.dart';
 import 'package:fluffychat/widgets/avatar.dart';
 import 'package:fluffychat/widgets/future_loading_dialog.dart';
 import 'package:fluffychat/widgets/mxc_image.dart';
+import 'package:fluffychat/widgets/mxc_image_viewer.dart';
 import '../../../config/app_config.dart';
 import '../../../utils/url_launcher.dart';
 import 'iframe_widget.dart';
@@ -419,6 +420,7 @@ class HtmlMessage extends StatelessWidget {
                     label: spoilerReason == null
                         ? L10n.of(context).spoilerText
                         : '${L10n.of(context).spoilerText}: $spoilerReason',
+                    mxcUrl: mxcUrl,
                     child: MxcImage(
                       uri: mxcUrl,
                       width: actualWidth,
@@ -583,46 +585,48 @@ class HtmlMessage extends StatelessWidget {
   }
 }
 
-class _InlineImageSpoiler extends StatefulWidget {
+class _InlineImageSpoiler extends StatelessWidget {
   final Widget child;
   final String label;
   final double width;
   final double height;
+  final Uri mxcUrl;
 
   const _InlineImageSpoiler({
     required this.child,
     required this.label,
     required this.width,
     required this.height,
+    required this.mxcUrl,
   });
-
-  @override
-  State<_InlineImageSpoiler> createState() => _InlineImageSpoilerState();
-}
-
-class _InlineImageSpoilerState extends State<_InlineImageSpoiler> {
-  bool _revealed = false;
 
   @override
   Widget build(BuildContext context) {
     final borderRadius = BorderRadius.circular(6);
 
-    return Material(
-      color: Colors.transparent,
-      clipBehavior: Clip.hardEdge,
-      borderRadius: borderRadius,
-      child: InkWell(
-        splashColor: Colors.transparent,
-        onTap: () => setState(() => _revealed = !_revealed),
-        child: SizedBox(
-          width: widget.width,
-          height: widget.height,
-          child: Stack(
-            fit: StackFit.expand,
-            children: [
-              widget.child,
-              if (!_revealed) MediaSpoilerOverlay(label: widget.label),
-            ],
+    return MediaSpoilerTapBuilder(
+      isSpoiler: true,
+      onOpen: () => showDialog(
+        context: context,
+        builder: (_) => MxcImageViewer(mxcUrl),
+      ),
+      builder: (context, isObscured, onTap) => Material(
+        color: Colors.transparent,
+        clipBehavior: Clip.hardEdge,
+        borderRadius: borderRadius,
+        child: InkWell(
+          splashColor: Colors.transparent,
+          onTap: onTap,
+          child: SizedBox(
+            width: width,
+            height: height,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                child,
+                if (isObscured) MediaSpoilerOverlay(label: label),
+              ],
+            ),
           ),
         ),
       ),
