2b1c99cd772f7234cd40128f3271beb31874a381
stevessr <steve-ssr@outlook.com>
2026-04-23 22:50:49 +0800

    feat: 添加 GoogleSansCode 和 GoogleSansCodeItalic 字体，并更新主题和消息组件以使用新字体

--- lib/pages/chat/events/message.dart
+++ lib/pages/chat/events/message.dart
@@ -746,7 +746,7 @@ class Message extends StatelessWidget {
                                                                 emojiTextStyle:
                                                                     const TextStyle(
                                                                   fontFamily:
-                                                                      'NotoColorEmoji',
+                                                                      'GoogleSansCode',
                                                                 ),
                                                                 emojiViewConfig:
                                                                     EmojiViewConfig(
